d8122d006162d9b6e1d75ff14fa2a59269b024a5
Matt West <mattantwest@gmail.com>
2016-10-19 08:53:48 +0100

    Added better error handling. Generalised the service SPF that gets added to the record.

--- server.js
+++ server.js
@@ -4,31 +4,53 @@ var app = express();
 
 var SPFService = require('./lib/spf_service.js');
 
-app.use( bodyParser.json() );
+app.use(bodyParser.json());
 
 app.post('/', function (req, res) {
-  var domain = req.body.domain;
+  var domain = req.body.domain,
+      serviceSpf = req.body.service_spf,
+      errors = [];
 
+  /**
+   * Basic Request Validation
+   */
   if (!domain) {
+    errors.push('Please specify a valid domain.');
+  }
+
+  if (!serviceSpf) {
+    errors.push('Please specify the service SPF record.');
+  }
+
+  if (errors.length) {
     return res.status(400).json({
-      status: 400,
-      message: 'Please specify a domain.'
+      errors: errors
     });
   }
 
   // Ensure there's no protocol on the domain.
   domain = domain.replace('https://', '').replace('http://', '');
 
-  SPFService.updateRecord(domain, 'spf.mtasv.net', function(result) {
-    res.json({
-      status: 200,
+  /**
+   * Check the DNS and respond with the modified SPF record.
+   */
+  SPFService.updateRecord(domain, serviceSpf, function(operation, record) {
+    res.status(200).json({
       domain: domain,
-      operation: result.operation,
-      spf: result.record
+      operation: operation,
+      record: record
+    });
+  }, function(errors) {
+    res.status(500).json({
+      errors: [ 'Whoops! An error occured. Make sure you specified a valid domain name.' ]
     });
   });
 });
 
+
+/**
+ * Start the web server.
+ */
 var port = process.env.PORT || 8080;
 
 app.listen(port, function () {
