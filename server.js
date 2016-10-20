91df13bea3ea89daad8f7953cc5825a13c59b846
Matt West <mattantwest@gmail.com>
2016-10-20 08:47:41 +0100

    Enable CORS

--- server.js
+++ server.js
@@ -5,6 +5,16 @@ var express = require('express'),
     fs = require('fs'),
     SPFService = require('./lib/spf_service.js');
 
+// Enable CORS
+var allowCrossDomain = function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+
+  next();
+}
+
+app.use(allowCrossDomain);
 app.use(bodyParser.json());
 
 app.get('/', function(req, res) {
